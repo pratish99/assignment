1365f152ecac8dda9828662aad18b9540b34f51d
Pratish Soni <sonipratish32@gmail.com>
2024-07-16 09:08:54 +0000

    updates

--- calculator/src/components/Calculator.js
+++ calculator/src/components/Calculator.js
@@ -5,13 +5,12 @@ const Calculator = ({ addToHistory }) => {
     const [data, setData] = useState("");
 
     const getValue =(event) =>{
-        console.log(event.target.value)
-        setData(data + event.target.value)
+        setData(data + event.target.value);
     }
     const calculation = () =>{
         try {
-            const result = evaluateExpression(data)
-            setData(result);
+            const result = evaluateExpression(data);
+            setData(result.toString());
             addToHistory(`${data} = ${result}`);
         } catch {
             setData("Error");
@@ -43,7 +42,7 @@ const Calculator = ({ addToHistory }) => {
         setData(data.slice(0, -1)); 
     }
     const clear = () =>{
-        setData("")
+        setData("");
     }
     const clearEnd = () => {
         const lastOperatorIndex = findLastOperatorIndex(data);
@@ -66,30 +65,30 @@ const Calculator = ({ addToHistory }) => {
         <div className='calculator'>
             <div className="container">
                 <input placeholder="0" value={data} readOnly />
-                <button onClick={clearEnd}>CE</button>
-                <button onClick={clear}>C</button>
-                <button onClick={back}>&#x232b;</button>
-                <button onClick={getValue} value="/">/</button>
+                <button className="action-button" onClick={clearEnd}>CE</button>
+                <button className="action-button" onClick={clear}>C</button>
+                <button className="action-button" onClick={back}>&#x232b;</button>
+                <button className="action-button" onClick={getValue} value="/">/</button>
 
                 <button onClick={getValue} value="7">7</button>
                 <button onClick={getValue} value="8">8</button>
                 <button onClick={getValue} value="9">9</button>
-                <button onClick={getValue} value="*">*</button>
+                <button className="action-button" onClick={getValue} value="*">*</button>
 
                 <button onClick={getValue} value="4">4</button>
                 <button onClick={getValue} value="5">5</button>
                 <button onClick={getValue} value="6">6</button>
-                <button onClick={getValue} value="-">-</button>
+                <button className="action-button" onClick={getValue} value="-">-</button>
 
                 <button onClick={getValue} value="1">1</button>
                 <button onClick={getValue} value="2">2</button>
                 <button onClick={getValue} value="3">3</button>
-                <button onClick={getValue} value="+">+</button>
+                <button className="action-button" onClick={getValue} value="+">+</button>
 
                 <button value="" disabled style={{ visibility: 'hidden' }}></button>
                 <button onClick={getValue} value="0">0</button>
-                <button onClick={getValue} value=".">.</button>
-                <button onClick={calculation}>=</button>
+                <button className="action-button" onClick={getValue} value=".">.</button>
+                <button className="action-button" onClick={calculation}>=</button>
             </div>   
         </div>
     </>
