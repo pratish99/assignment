5cf363fe1392804ab94a9f96c39c3d15781ee77b
Pratish Soni <sonipratish32@gmail.com>
2024-07-16 06:52:14 +0000

    calculator functionality

---
+++ calculator/src/components/Calculator.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const Calculator = () => {
+    const [data, setData] = useState("");
+
+    const getValue =(event) =>{
+        console.log(event.target.value);
+        setData(data.concat(event.target.value))
+    }
+    const calculation = () =>{
+        setData(eval(data).toString())
+    }
+    const back = () =>{
+        setData(data.slice(0,-1))
+    }
+    const clear = () =>{
+        setData("")
+    }
+    return (
+    <>
+        <div className="container">
+            <div>
+                <input placeholder='0' value={data}/>
+            </div>
+            <br/>
+            <button onClick={clear}>C</button>{/* clear end button */}
+            <button onClick={clear}>C</button>
+            <button onClick={back}>Back</button>
+            <button onClick={getValue} value="/">/</button>
+
+            <button onClick={getValue} value="7">7</button>
+            <button onClick={getValue} value="8">8</button>
+            <button onClick={getValue} value="9">9</button>
+            <button onClick={getValue} value="*">*</button>
+
+            <button onClick={getValue} value="4">4</button>
+            <button onClick={getValue} value="5">5</button>
+            <button onClick={getValue} value="6">6</button>
+            <button onClick={getValue} value="-">-</button>
+
+            <button onClick={getValue} value="1">1</button>
+            <button onClick={getValue} value="2">2</button>
+            <button onClick={getValue} value="3">3</button>
+            <button onClick={getValue} value="+">+</button>
+
+            <button onClick={getValue} value="0">0</button> {/* To be changed*/}
+            <button onClick={getValue} value="0">0</button>
+            <button onClick={getValue} value=".">.</button>
+            <button onClick={calculation}>=</button>
+        </div>    
+    </>
+    )
+}
+
+export default Calculator
